1f60635878576ec28a8a7abc6b84f657d5fef7d5
edward <edwardez@users.noreply.github.com>
2018-03-14 21:51:01 -0400

    update winston logger format(more human-readable now!)

--- server/config/components/logger.js
+++ server/config/components/logger.js
@@ -1,16 +1,8 @@
 const joi = require('joi');
 const winston = require('winston');
-const path = require('path');
+
 const fs = require('fs');
 
-const {
-  combine, timestamp, splat,
-} = winston.format;
-
-// create logs folder if it doesn't exist
-if (!fs.existsSync('logs')) {
-  fs.mkdirSync('logs');
-}
 
 const envVarsSchema = joi.object({
   LOGGER_LEVEL: joi.string()
@@ -37,33 +29,9 @@ const config = {
   },
 };
 
-winston.configure({
-  format: combine(
-    winston.format.colorize({ all: true }),
-    splat(),
-    timestamp(),
-    winston.format.simple(),
-  ),
-  level: config.logger.level,
-  transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    // TODO: write log file according to path in .env
-    // TODO: improve log conf
-    new winston.transports.File({ filename: path.normalize(`${__dirname}/../../logs/error.log`), level: 'error', eol: '\r\n' }),
-    new winston.transports.File({ filename: path.normalize(`${__dirname}/../../logs/combined.log`), eol: '\r\n' }),
-    new winston.transports.Console({
-    }),
-  ],
-  // exitOnError: true,
-});
-
-// winston.exceptions.handle(new winston.transports.File({
-// filename: path.normalize(`${__dirname}/../../logs/exceptions.log`), eol: '\r\n' }));
-
 
 if (!config.logger.enabled) {
+  // TODO: a better way to remove all loggers
   winston.clear(); // Remove all transports
 }
 module.exports = config;

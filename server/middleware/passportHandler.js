42c9be9bf61350f1ea3eec59f4e6e3c8beaae5f8
edward <edwardez@users.noreply.github.com>
2018-03-17 18:21:59 -0400

    Move getUserProfile handler to a separated file(potential conflict with passport native method) Refactor getUserProfile method to express-standard async middleware style

--- server/middleware/passportHandler.js
+++ server/middleware/passportHandler.js
@@ -2,9 +2,7 @@ const passport = require('passport');
 const OAuth2Strategy = require('passport-oauth2').Strategy;
 const config = require('../config/components/passport');
 const request = require('request');
-const requestPromise = require('request-promise');
-const logger = require('../utils/logger')(module);
-const joi = require('joi');
+
 
 // oauth strategy for bangumi
 const bangumiOauth = new OAuth2Strategy(
@@ -44,30 +42,7 @@ bangumiOauth.userProfile = function userProfile(accessToken, done) {
   );
 };
 
-/**
- * verify that profile is valid
- * profile is returned from server, and it must:
- * have a valid access_token, and client_id should be the same as what's recorded
- * and user_id cannot be empty
- * in .env file
- * @param userProfile user profile object
- * @param clientId oauth client id
- * @returns {*}
- */
-function verifyAccessToken(userProfile, clientId) {
-  const profileSchema = joi.object({
-    access_token: joi.string().required(),
-    user_id: joi.required(),
-    client_id: joi.string().valid(clientId).required(),
-  }).unknown().required();
 
-  const { error, value: profileVars } = joi.validate(userProfile, profileSchema);
-  if (error) {
-    throw new Error(`Config validation error: ${error.message}`);
-  }
-
-  return profileVars;
-}
 
 passport.use('bangumi-oauth', bangumiOauth);
 
@@ -78,22 +53,6 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((user, done) => {
   done(null, user);
 });
-
-
-passport.getUserProfile = async function getUserProfile(accessToken) {
-  let response;
-  let data;
-
-  try {
-    response = await requestPromise.post(`${config.passport.oauth.bangumi.tokenStatusURL}?app_id=${config.passport.oauth.bangumi.clientID}2&access_token=${accessToken}`);
-    data = verifyAccessToken(JSON.parse(response), config.passport.oauth.bangumi.clientID);
-  } catch (err) {
-    logger.error('Response cannot be parsed or verified: %o', response);
-    logger.error(err);
-  }
-
-  return data;
-};
 
 
 module.exports = passport;
---
+++ server/middleware/userProfileHandler.js
@@ -0,0 +1,51 @@
+const requestPromise = require('request-promise');
+const logger = require('../utils/logger')(module);
+const joi = require('joi');
+const { CustomError } = require('../middleware/errorHandler');
+const config = require('../config/components/passport');
+
+/**
+ * verify that profile is valid
+ * profile is returned from server, and it must:
+ * have a valid access_token, and client_id should be the same as what's recorded
+ * and user_id cannot be empty
+ * in .env file
+ * @param userProfile user profile object
+ * @param clientId oauth client id
+ * @returns {*}
+ */
+function verifyAccessToken(userProfile, clientId) {
+  const profileSchema = joi.object({
+    access_token: joi.string().required(),
+    user_id: joi.required(),
+    client_id: joi.string().valid(clientId).required(),
+  }).unknown().required();
+
+  const { error, value: profileVars } = joi.validate(userProfile, profileSchema);
+  if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+  }
+
+  return profileVars;
+}
+
+const getUserProfile = async function getUserProfile(req, res, next) {
+  const { accessToken } = req.body;
+  req.bangumin = req.bangumin || {};
+  let response;
+  let userProfile;
+
+  try {
+    response = await requestPromise.post(`${config.passport.oauth.bangumi.tokenStatusURL}?app_id=${config.passport.oauth.bangumi.clientID}2&access_token=${accessToken}`);
+    userProfile = verifyAccessToken(JSON.parse(response), config.passport.oauth.bangumi.clientID);
+    req.bangumin.userProfile = userProfile;
+  } catch (err) {
+    logger.error('Response cannot be parsed or verified: %o', response);
+    logger.error(err);
+    return next(new CustomError('response_invalid', 'Response cannot be verified: use your bangumi account to log in again.'));
+  }
+
+  return next();
+};
+
+module.exports = getUserProfile;

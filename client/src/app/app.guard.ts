73abf0cb603ab8d9d02beebecb22ec6d0645a993
edward <edwardez@users.noreply.github.com>
2018-03-18 23:53:43 -0400

    Update app guard

--- client/src/app/app.guard.ts
+++ client/src/app/app.guard.ts
@@ -2,17 +2,28 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
+import {AuthenticationService} from './shared/services/auth.service';
 
 @Injectable()
 export class AppGuard implements CanActivate {
 
   constructor(
-    private router: Router
+    private authService: AuthenticationService,
+      private router: Router
   ) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    return this.authService.userSubject
+      .pipe(
+        map(user => {
+          if (user === null) {
+            this.router.navigate(['/login']);
+          }
+          return user !== null;
+        })
+      );
+
   }
 }

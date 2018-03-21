c89675ee45f3209c0f8176b3f83588bcd663527e
edward <edwardez@users.noreply.github.com>
2018-03-21 17:34:14 -0400

    Convert http link to https

--- client/src/app/shared/models/BangumiUser.ts
+++ client/src/app/shared/models/BangumiUser.ts
@@ -6,7 +6,7 @@ export class BangumiUser implements Serializable<BangumiUser> {
 
   deserialize(input) {
     this.user_id = input.id;
-    this.avatar = input.avatar.large;
+    this.avatar = input.avatar.large.replace(/^http:/, 'https:'); // convert http link to https
     this.nickname = input.nickname;
     this.username = input.username;
     return this;

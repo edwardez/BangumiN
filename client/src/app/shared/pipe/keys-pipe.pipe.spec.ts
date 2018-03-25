57107033233ed43c0d42ba309ccd741a624df2b0
edward <edwardez@users.noreply.github.com>
2018-03-24 20:18:24 -0400

    Add a pipe to iterate through object

---
+++ client/src/app/shared/pipe/keys-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { KeysPipePipe } from './keys-pipe.pipe';
+
+describe('KeysPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new KeysPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ client/src/app/shared/pipe/keys-pipe.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'keys'
+})
+export class KeysPipePipe implements PipeTransform {
+
+  transform(value, args: string[]): any {
+    const keys = [];
+    for (const key in value) {
+      if (value.hasOwnProperty(key)) {
+        keys.push({key: key, value: value[key]});
+      }
+    }
+    return keys;
+  }
+
+}

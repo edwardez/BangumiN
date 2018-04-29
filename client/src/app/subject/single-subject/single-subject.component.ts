7d5629954bbbc11856001da90a52d8582235a185
edward <edwardez@users.noreply.github.com>
2018-04-29 01:57:07 -0700

    Add subject page with some basic info

--- client/src/app/subject/single-subject/single-subject.component.ts
+++ client/src/app/subject/single-subject/single-subject.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {BangumiSubjectService} from '../../shared/services/bangumi/bangumi-subject.service';
+import {filter, switchMap} from 'rxjs/operators';
+import {SubjectSmall} from '../../shared/models/subject/subject-small';
+import {SubjectLarge} from '../../shared/models/subject/subject-large';
 
 @Component({
   selector: 'app-single-subject',
@@ -9,12 +12,24 @@ import {BangumiSubjectService} from '../../shared/services/bangumi/bangumi-subje
 })
 export class SingleSubjectComponent implements OnInit {
 
+  subject: SubjectLarge;
+
   constructor(private route: ActivatedRoute,
               private bangumiSubjectService: BangumiSubjectService
               ) { }
 
   ngOnInit() {
-
+    this.route
+      .params
+      .pipe(
+        filter(params => params['id'] !== undefined),
+        switchMap(params => {
+          return this.bangumiSubjectService.getSubject( params['id'], 'large');
+        }
+      ))
+      .subscribe( res => {
+        this.subject = res;
+      });
   }
 
 }

20d3d0891622942c95eae6c1e959f48d1d510f6f
edward <edwardez@users.noreply.github.com>
2018-05-03 23:12:04 -0700

    Implement ControlValueAccessor in star rating component

--- client/src/app/common/star-rating/star-rating.component.ts
+++ client/src/app/common/star-rating/star-rating.component.ts
@@ -1,21 +1,27 @@
-// inspired by https://github.com/ERS-HCL/star-rating-angular-material
+// inspired by https://github.com/ERS-HCL/star-rating-angular-material with heavy modification
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'app-star-rating',
+  providers:  [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: StarRatingComponent,
+    multi: true
+  }],
   templateUrl: './star-rating.component.html',
   styleUrls: ['./star-rating.component.scss']
 })
-export class StarRatingComponent implements OnInit {
+export class StarRatingComponent implements OnInit, ControlValueAccessor {
 
   @Input('currentRating') private currentRating: number;
   @Input('starCount') private starCount: number;
   @Input('isEditable') private isEditable: boolean;
   @Output() private ratingUpdated = new EventEmitter();
 
-  ratingToBeStore: number;
-
+  private ratingToBeStore: number;
   private ratingArray = [];
+  private propagateChange = (_: any) => {};
 
   constructor() {
   }
@@ -31,23 +37,21 @@ export class StarRatingComponent implements OnInit {
   onClickSingleRatingStar(rating: number) {
     if (!this.isEditable) { return false; }
 
-    this.ratingUpdated.emit(rating);
+    this.currentRating = rating;
     this.ratingToBeStore = rating;
+    this.propagateChange(rating);
+    this.ratingUpdated.emit(this.ratingToBeStore);
     return false;
   }
 
-  onHoverEnterSingleRatingStar(ratingId: number) {
+  onHoverEnterSingleRatingStar(rating: number) {
     if (!this.isEditable) { return false; }
-
-    this.ratingUpdated.emit(ratingId);
-
+    this.currentRating = rating;
   }
 
   onHoverLeaveRatingArea() {
     if (!this.isEditable) { return false; }
-
     this.currentRating = this.ratingToBeStore;
-    this.ratingUpdated.emit(this.ratingToBeStore);
   }
 
   showIcon(index: number) {
@@ -55,6 +59,27 @@ export class StarRatingComponent implements OnInit {
       return 'star';
     } else {
       return 'star_border';
+    }
+  }
+
+
+
+  registerOnChange(fn: any): void {
+    if (this.isEditable) {
+      this.propagateChange = fn;
+    }
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isEditable = !isDisabled;
+  }
+
+  writeValue(value: any): void {
+    if (this.isEditable) {
+      this.currentRating = value;
     }
   }
 

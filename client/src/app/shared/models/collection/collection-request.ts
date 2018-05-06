61b57856699f8d3e4f7a10178b36247d141e0791
edward <edwardez@users.noreply.github.com>
2018-05-05 23:16:22 -0700

    Update collection model: 1. Change private to privacy: avoid using reserved keyword 2. Parse tags to [] if the response is ['']

--- client/src/app/shared/models/collection/collection-request.ts
+++ client/src/app/shared/models/collection/collection-request.ts
@@ -3,11 +3,11 @@ import {CollectionStatusType} from '../../enums/collection-status-type';
 export class CollectionRequest {
   status: CollectionStatusType;
   comment?: string;
-  tags?: string;
+  tags?: string[];
   rating?: number;
   privacy?: number;
 
-  constructor(status: CollectionStatusType, comment = '', tags = '', rating = 0, privacy = 0) {
+  constructor(status: CollectionStatusType, comment = '', tags = [], rating = 0, privacy = 0) {
     this.status = status;
     this.comment = comment;
     this.tags = tags;
--- client/src/app/shared/models/collection/collection-response.ts
+++ client/src/app/shared/models/collection/collection-response.ts
@@ -13,7 +13,9 @@ export class CollectionResponse  implements Serializable<CollectionResponse> {
   rating?: number;
   /** 评论 */
   comment?: string;
-  private?: number;
+
+  /** only visible to your self*/
+  privacy?: number;
   /** 标签 */
   tags?: string[];
   /**
@@ -29,12 +31,26 @@ export class CollectionResponse  implements Serializable<CollectionResponse> {
 
   user?: BangumiUser;
 
+  static parseTags(input) {
+    let tags: string[];
+    if (input.tag) {
+      tags = input.tag;
+    } else {
+      if (input.tags) {
+        tags = input.tags;
+      } else {
+        tags = [];
+      }
+    }
+    return tags.filter(tag => tag.length >= 1);
+  }
+
   constructor() {
     this.status = new CollectionStatus();
     this.rating = 0;
     this.comment = '';
-    this.private = 0;
-    this.tags = [''];
+    this.privacy = 0;
+    this.tags = [];
     this.epStatus = 0;
     this.lastTouch = 0;
     this.user = new BangumiUser();
@@ -45,11 +61,13 @@ export class CollectionResponse  implements Serializable<CollectionResponse> {
     this.rating = input.rating === undefined ? 0 : input.rating;
     // in case comment is  longer than maximum: truncate it
     this.comment = input.comment === undefined ? '' : input.comment.substring(0, environment.commentMaxLength);
-    this.private = input.private === undefined ? 0 : input.private;
-    this.tags = input.tag === undefined ?  (input.tags === undefined ? [''] : input.tags) : input.tag;
+    this.privacy = input.private === undefined ? 0 : input.private;
+    this.tags = CollectionResponse.parseTags(input);
     this.epStatus = input.ep_status === undefined ? 0 : input.ep_status;
     this.lastTouch = input.lasttouch === undefined ? 0 : input.lasttouch;
     this.user = input.user === undefined ? new BangumiUser() : input.user;
     return this;
   }
+
+
 }

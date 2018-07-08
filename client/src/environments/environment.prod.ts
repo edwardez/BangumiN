ea816f0522cff5aa766dbb7292a325bda5117744
edward <edwardez@users.noreply.github.com>
2018-07-08 12:58:59 -0700

    Change backend url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   FRONTEND_URL: 'https://bangumin.tv',
-  BANGUMI_API_URL: 'https://bangumin.tv/proxy/api/bangumi',
-  BANGUMI_OAUTH_URL: 'https://bangumin.tv/proxy/oauth/bangumi',
-  BACKEND_API_URL: 'https://bangumin.tv/api',
-  BACKEND_AUTH_URL: 'https://bangumin.tv/auth',
-  BACKEND_OAUTH_URL: 'https://bangumin.tv/oauth',
+  BANGUMI_API_URL: 'https://api.bangumin.tv/proxy/api/bangumi',
+  BANGUMI_OAUTH_URL: 'https://api.bangumin.tv/proxy/oauth/bangumi',
+  BACKEND_API_URL: 'https://api.bangumin.tv',
+  BACKEND_AUTH_URL: 'https://api.bangumin.tv/auth',
+  BACKEND_OAUTH_URL: 'https://api.bangumin.tv/oauth',
   BANGUMI_APP_ID: 'bgm1065aa2020942ce2',
-  whitelistedDomains: ['bangumin.tv'],
-  blacklistedRoutes: ['bangumin.tv/api', 'bangumin.tv/oauth', 'bangumin.tv/auth'],
+  whitelistedDomains: ['api.bangumin.tv'],
+  blacklistedRoutes: ['api.bangumin.tv/oauth', 'api.bangumin.tv/auth'],
   availableLanguage: {
     'en-US': 'English',
     'zh-Hans': '简体中文'

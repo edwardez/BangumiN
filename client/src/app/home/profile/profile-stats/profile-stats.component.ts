d518403cd83c44388b186e3f2296d24f04295911
puranzhang <puranzhang@gmail.com>
2018-08-05 16:09:46 -0700

    Add user profile stats component(in progress)

--- client/src/app/home/profile/profile-stats/profile-stats.component.ts
+++ client/src/app/home/profile/profile-stats/profile-stats.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {BangumiUserService} from "../../../shared/services/bangumi/bangumi-user.service";
+import {BangumiUserService} from '../../../shared/services/bangumi/bangumi-user.service';
 import * as _ from 'lodash';
 
 @Component({
@@ -32,7 +32,7 @@ export class ProfileStatsComponent implements OnInit {
     this.gradient = false;
     this.showLegend = true;
     this.showGridLines = true;
-
+    // todo: translate legend and label
     this.xAxisLabel = 'Score';
     this.showXAxisLabel = true;
     this.showXAxis = true;
@@ -42,7 +42,7 @@ export class ProfileStatsComponent implements OnInit {
     this.showYAxis = true;
 
     this.colorScheme = {
-      domain: ['#d53e4f', '#f46d43', '#fdae61', '#fee08b', '#ffffbf', '#e6f598', '#abdda4', '#66c2a5', '#3288bd', "#2361bd"]
+      domain: ['#d53e4f', '#f46d43', '#fdae61', '#fee08b', '#ffffbf', '#e6f598', '#abdda4', '#66c2a5', '#3288bd', '#2361bd']
     };
 
     this.typeList = ['All', 'Real', 'Anime'];
@@ -50,22 +50,22 @@ export class ProfileStatsComponent implements OnInit {
   }
 
   ngOnInit() {
-    let arr = this.banguminUserService.getUserProfileStats('hi');
+    const arr = this.banguminUserService.getUserProfileStats('hi');
     this.groupAndCountByRate(arr);
   }
 
   switchType() {
     // value can't be exact 0 due to https://github.com/swimlane/ngx-charts/issues/498
     // tmp hack: use 0.0000001 instead
-    let arr = this.banguminUserService.getUserProfileStats('hi', this.selectedTypeList);
+    const arr = this.banguminUserService.getUserProfileStats('hi', this.selectedTypeList);
     this.groupAndCountByRate(arr);
   }
 
   private groupAndCountByRate(arr) {
     // TODO: fixed xAxis ticks
-    let xAxisTicks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-    let countedArr = _.map(_.countBy(arr, "rate"), (val, key) => ({name: key, value: val}));
-    let diff = _.difference(xAxisTicks, countedArr.map(t => t.name));
+    const xAxisTicks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+    const countedArr = _.map(_.countBy(arr, 'rate'), (val, key) => ({name: key, value: val}));
+    const diff = _.difference(xAxisTicks, countedArr.map(t => t.name));
     if (diff.length !== 0) {
       diff.forEach((axis) => {
         countedArr.push({name: axis, value: 0.0000001});

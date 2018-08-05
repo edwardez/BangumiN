211a6368ed0b699eec051651c0ca64cb14c09fc9
edward <edwardez@users.noreply.github.com>
2018-08-05 00:35:39 -0700

    Add dynamoose, connect-dynamodb and relevant type definitions

---
+++ server/src/types/connect-dynamodb.d.ts
@@ -0,0 +1,37 @@
+import * as session from 'express-session';
+import * as express from 'express';
+import * as AWS from 'aws-sdk';
+
+export = connect_dynamodb;
+
+declare function connect_dynamodb(connect: (options?: session.SessionOptions) => express.RequestHandler): any;
+
+declare namespace connect_dynamodb {
+
+  export interface configOptions {
+    // Optional DynamoDB table name, defaults to 'sessions'
+    table?: string;
+
+    // Optional path to AWS credentials and configuration file
+    AWSConfigPath?: string;
+
+    // Optional JSON object of AWS credentials and configuration
+    AWSConfigJSON?: {
+      accessKeyId: string,
+      secretAccessKey: string,
+      region: string,
+    };
+    // Optional client for alternate endpoint, such as DynamoDB Local
+    client?: AWS.DynamoDB.DocumentClient;
+
+    // Optional ProvisionedThroughput params, defaults to 5
+    readCapacityUnits?: number;
+    writeCapacityUnits?: number;
+  }
+
+  // noinspection TsLint
+  function DynamoDBStore(options?: configOptions): void;
+
+  const prototype: {};
+//
+}

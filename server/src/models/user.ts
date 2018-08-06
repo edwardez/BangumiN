3e2deda1ad8ce6e036515b45d7e46a0f4b197b02
edward <edwardez@users.noreply.github.com>
2018-08-05 23:03:43 -0700

    Update user model

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -5,59 +5,70 @@ import Logger from '../utils/logger';
 
 const logger = Logger(module);
 
-export interface IUserModel extends dynamoose.Model<IUserModel> {
+export interface UserSchema {
   id: string;
+  loggedInAt: number;
   appLanguage?: string;
   bangumiLanguage?: string;
 }
 
-const userSchema = new dynamoose.Schema(
-  {
-    id: {
-      hashKey: true,
-      lowercase: true,
-      required: true,
-      type: String,
-      trim: true,
-      validate: (v: string) => Joi.string().regex(/^[0-9]+$/).validate(v).error === null,
-    },
-    appLanguage: {
-      default: 'zh-Hans',
-      type: String,
-      trim: true,
-      validate: (v: string) => {
-        return Joi.string().valid(['en-US', 'zh-Hans']).validate(v).error === null;
-      },
-    },
-    bangumiLanguage: {
-      default: 'original',
-      type: String,
-      trim: true,
-      validate: (v: string) => {
-        return Joi.string().valid(['original', 'zh-Hans']).validate(v).error === null;
-      },
-    },
+export interface UserModel extends dynamoose.Model<UserModel>, UserSchema {
+}
 
+const userSchema = new dynamoose.Schema({
+  id: {
+    hashKey: true,
+    lowercase: true,
+    required: true,
+    type: String,
+    trim: true,
+    validate: (v: string) => Joi.string().regex(/^[0-9]+$/).validate(v).error === null,
   },
-  {
-    timestamps: true,
-  });
+  appLanguage: {
+    default: 'zh-Hans',
+    type: String,
+    trim: true,
+    validate: (v: string) => {
+      return Joi.string().valid(['en-US', 'zh-Hans']).validate(v).error === null;
+    },
+  },
+  bangumiLanguage: {
+    default: 'original',
+    type: String,
+    trim: true,
+    validate: (v: string) => {
+      return Joi.string().valid(['original', 'zh-Hans']).validate(v).error === null;
+    },
+  },
+  loggedInAt: {
+    default: (new Date).getTime(),
+    type: Number,
+    trim: true,
+    validate: (v: number) => {
+      return Joi.date().timestamp().validate(v).error === null;
+    },
+  },
+
+}, {
+  timestamps: true,
+});
 
 export const userModel = dynamoose.model(`BangumiN_${config.env}_users`, userSchema);
 
-export class UserModel {
+export class User {
 
-  private userModel: IUserModel;
+  private userModel: UserModel;
 
-  constructor(userModel: IUserModel) {
+  constructor(userModel: UserModel) {
     this.userModel = userModel;
   }
 
-  get id(): string {
-    return this.userModel.id;
-  }
-
-  static createUser(id: string, appLanguage?: string): Promise<dynamoose.Model<IUserModel>> {
+  /**
+   * create a new user, return error upon rejection
+   * @param id user primary key id
+   * @param appLanguage app main language
+   */
+  static createUser(id: string, appLanguage?: string): Promise<dynamoose.Model<UserModel>> {
     logger.info(`Create new user: ${id}`);
 
     const userData = appLanguage === undefined ? {id: id.toString()} : {id: id.toString(), appLanguage: appLanguage.toString()};
@@ -69,34 +80,41 @@ export class UserModel {
     });
   }
 
-  static findOrCreateUser(id: string): Promise<dynamoose.Model<IUserModel>> {
-    return userModel.get(id).then((existedUser: IUserModel) => {
-        if (existedUser === undefined) {
-          logger.info(`User: ${id} is not in database`);
-          return UserModel.createUser(id);
-        }
-        logger.info(`Bangumi user:${id}, database id:${existedUser.id} logged in`);
-        return existedUser;
-      },
-    ).catch((error) => {
-      if (error === undefined) {
+  /**
+   * find user settings and return, if user doesn't exist, create user and return a new user model as a promise
+   * @param userID user id, can be string or number, number will be converted to string
+   */
+  static findOrCreateUser(userID: string | number): Promise<dynamoose.Model<UserModel>> {
+    const id = userID.toString();
+    return userModel.get(id).then((existedUser: UserModel) => {
+      if (existedUser === undefined) {
+        logger.info(`User: ${id} is not in database`);
+        return User.createUser(id);
       }
+      logger.info(`Bangumi user:${id}, database id:${existedUser.id} logged in`);
+      return existedUser;
+    }).catch((error) => {
       logger.error(`Failed to execute find user step for id:${id}`);
       logger.error(error.stack);
       return error;
     });
   }
 
-  static updateUser(userSettings: IUserModel): Promise<dynamoose.Model<IUserModel>> {
-    // id cannot be updated, exclude it
-    const newUserSettings = Object.assign(userSettings);
-    delete newUserSettings.id;
-    return userModel.update(userSettings.id, newUserSettings).then(
-      (updatedUserSettings) => {
-        logger.info('Successfully updated user settings');
-        return updatedUserSettings;
-      },
-    ).catch((error) => {
+  /**
+   * Update user settings
+   * @param userSettings user settings
+   */
+  static updateUser(userSettings: UserSchema): Promise<dynamoose.Model<UserModel>> {
+    const userId = userSettings.id.toString();
+    delete userSettings.id;
+    logger.info('Trying to update user %s\'s settings as %o', userId, userSettings);
+    // id shouldn't be updated, exclude it
+    return userModel.update({id: userId}, userSettings).then((updatedUser) => {
+      logger.info('Succeeded to update user settings');
+      return updatedUser;
+    }).catch((error) => {
+      logger.error('Failed to update user settings');
+      logger.error(error.stack);
       return error;
     });
   }

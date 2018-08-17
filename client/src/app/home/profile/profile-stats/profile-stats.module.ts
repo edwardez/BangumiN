dbe5289bd527a1ac56b3a6b799739248235d0fba
puranzhang <puranzhang@gmail.com>
2018-08-17 01:22:23 -0700

    Add user profile stats component(in progress)

--- client/src/app/home/profile/profile-stats/profile-stats.module.ts
+++ client/src/app/home/profile/profile-stats/profile-stats.module.ts
@@ -2,7 +2,8 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {ProfileStatsRoutingModule} from './profile-stats-routing.module';
-import {NgxChartsModule} from '@swimlane/ngx-charts';
+// import {NgxChartsModule} from '@swimlane/ngx-charts';
+import {BarChartModule, LineChartModule} from '@swimlane/ngx-charts';
 import {ProfileStatsComponent} from './profile-stats.component';
 import {BanguminSharedModule} from '../../../../bangumin-shared.module';
 
@@ -11,7 +12,9 @@ import {BanguminSharedModule} from '../../../../bangumin-shared.module';
     CommonModule,
     BanguminSharedModule,
     ProfileStatsRoutingModule,
-    NgxChartsModule,
+    // NgxChartsModule,
+    LineChartModule,
+    BarChartModule
   ],
   declarations: [
     ProfileStatsComponent,

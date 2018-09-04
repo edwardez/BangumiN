c3c4b72de1143d3f44183af4020c2e3cf1cd720a
edward <edwardez@users.noreply.github.com>
2018-09-03 19:27:20 -0700

    Update pipeline initialization script

--- pipeline/execute_pipeline.sh
+++ pipeline/execute_pipeline.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-# usage ./execute_pipeline.sh [pipeline name] [sync type]
 set -e
 
 sudo yum groupinstall "Development Tools" -y
 sudo yum install git -y
+git clone https://github.com/edwardez/BangumiN.git && cd BangumiN
+# for now we'll use code from develop branch, in the future we should use the master branch
+git checkout origin/develop && cd pipeline
 
 wget https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -f -p $HOME/miniconda
@@ -12,4 +14,3 @@ export PATH="$HOME/miniconda/bin:$PATH"
 pip install -r requirements.txt
 
 python run_task.py $1 $2
-

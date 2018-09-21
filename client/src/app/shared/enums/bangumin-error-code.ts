f27e4ab8b640a900a5bdff63128584c8c84ac849
edward <edwardez@users.noreply.github.com>
2018-09-21 01:54:52 -0700

    Add an interceptor on backend to notify error

---
+++ client/src/app/shared/enums/bangumin-error-code.ts
@@ -0,0 +1,9 @@
+export enum BanguminErrorCode {
+  UnclassifiedInternalError = 1,
+  ValidationError = 2,
+  BangumiServerResponseError = 3,
+  RequestResourceNotFoundError = 4,
+  RDSResponseError = 5,
+  NoSQLResponseError = 6,
+  UnauthorizedError = 7
+}
--- client/src/app/shared/interceptors/interceptors.module.ts
+++ client/src/app/shared/interceptors/interceptors.module.ts
@@ -3,6 +3,7 @@ import {OauthInterceptor} from './oauth.interceptor';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {CacheInterceptor} from './cache/cache.interceptor';
 import {CustomXsrfInterceptor} from './xsrf/custom-xsrf.interceptor';
+import {ErrorNotificationInterceptor} from './error-handler/error-notification.interceptor';
 
 // TODO: remove this interceptor once https://github.com/auth0/angular2-jwt/issues/427 is solved
 // currently it's needed to added different header here since angular-jwt doesn't support change token according to domain routes
@@ -31,6 +32,11 @@ export class InterceptorsModule {
         {
           provide: HTTP_INTERCEPTORS,
           useClass: CustomXsrfInterceptor,
+          multi: true
+        },
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: ErrorNotificationInterceptor,
           multi: true
         }
       ]

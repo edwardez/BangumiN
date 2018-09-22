7a8b719f04426034a20479cef9aeaebb893b444e
edward <edwardez@users.noreply.github.com>
2018-09-22 14:01:38 -0700

    Prevent unauthorized access to spoilers page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -56,6 +56,7 @@ const routes: Routes = [
 
       {
         path: 'timeline/spoilers',
+        canActivate: [AppGuard],
         loadChildren: 'app/user/timeline/spoilers/spoilers.module#SpoilersModule'
       },
       {

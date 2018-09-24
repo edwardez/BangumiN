334fa75d15e5df4821e44e8d80a771ca951bc23f
edward <edwardez@users.noreply.github.com>
2018-09-24 01:42:34 -0700

    Rewrite the log in process since cookie is not shared in pwa's external browser(but in-app browser shares cookie with the main pwa) and update website logo and favicon(Originally it was designed by me in 1 minute, the new one was designed by Andrew in 2 minutes)

--- client/src/app/auth/login-bangumi/login-bangumi.component.ts
+++ client/src/app/auth/login-bangumi/login-bangumi.component.ts
@@ -1,11 +1,9 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {environment} from '../../../environments/environment';
 import {MatDialog, MatSnackBar} from '@angular/material';
-import {BangumiAuthWaitDialogComponent} from './bangumi-auth-wait-dialog/bangumi-auth-wait-dialog.component';
 import {TranslateService} from '@ngx-translate/core';
 import {AuthenticationService} from '../../shared/services/auth.service';
-import {catchError, finalize, switchMap, takeUntil} from 'rxjs/operators';
-import {Subject, throwError} from 'rxjs';
+import {Subject} from 'rxjs';
 import {Router} from '@angular/router';
 import {StorageService} from '../../shared/services/storage.service';
 
@@ -16,59 +14,9 @@ import {StorageService} from '../../shared/services/storage.service';
 })
 export class LoginBangumiComponent implements OnInit, OnDestroy {
 
-  receiveMessageHandler;
-  bangumiAuthWaitDialog;
-  openedBangumiPopup;
+  backendOauthUrl = `${environment.BACKEND_OAUTH_URL}/bangumi`;
   disableLoginButton = false;
   private ngUnsubscribe: Subject<void> = new Subject<void>();
-
-  receiveMessage: any = (event: any) => {
-    if (event && event.data && event.data.type === 'bangumiCallBack') {
-      if (this.bangumiAuthWaitDialog) {
-        this.bangumiAuthWaitDialog.close();
-      }
-      if (this.openedBangumiPopup) {
-        this.openedBangumiPopup.close();
-      }
-      this.removePostMessageListener();
-
-      const bangumiCallBackData = event.data;
-      let loginResultTranslationLabel: string;
-      if (bangumiCallBackData['result'] === 'success') {
-        this.authenticationService.verifyAndSetBangumiActivationInfo(bangumiCallBackData['userInfo'])
-          .pipe(
-            switchMap(
-              res => {
-                loginResultTranslationLabel = 'login.loginSuccess';
-                this.router.navigate(['/welcome']);
-                return this.translateService.get(loginResultTranslationLabel);
-              }
-            ),
-            catchError(error => {
-              loginResultTranslationLabel = 'login.loginFailure';
-              return throwError(error);
-            }),
-            finalize(() => {
-              this.translateService.get(loginResultTranslationLabel).subscribe(res => {
-                this.snackBar.open(res, '', {
-                  duration: 2000
-                });
-              });
-            }),
-            takeUntil(this.ngUnsubscribe),
-          )
-          .subscribe(res => {
-          });
-      } else {
-        loginResultTranslationLabel = 'login.loginFailure';
-        this.translateService.get(loginResultTranslationLabel).subscribe(res => {
-          this.snackBar.open(res, '', {
-            duration: 10000
-          });
-        });
-      }
-    }
-  };
 
   constructor(private authenticationService: AuthenticationService,
               public dialog: MatDialog,
@@ -82,28 +30,12 @@ export class LoginBangumiComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.receiveMessageHandler = this.receiveMessage.bind(this);
   }
 
   ngOnDestroy(): void {
     // unsubscribe, we can also first() in subscription
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
-  }
-
-  redirectToBangumi() {
-    window.addEventListener('message', this.receiveMessageHandler, false);
-    this.openedBangumiPopup = window.open(`${environment.BACKEND_OAUTH_URL}/bangumi`);
-    this.bangumiAuthWaitDialog = this.dialog.open(BangumiAuthWaitDialogComponent,
-      {
-        data: {
-          receiveMessageHandler: this.receiveMessageHandler
-        }
-      });
-  }
-
-  removePostMessageListener() {
-    window.removeEventListener('message', this.receiveMessageHandler, false);
   }
 
 }

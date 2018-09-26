7bb7b391b4c628a23d985f25678cbaaa70e65ce0
edward <edwardez@users.noreply.github.com>
2018-09-26 01:47:50 -0700

    Invalidate user session during logout

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,17 @@
 import * as express from 'express';
+import config from '../config';
 
 const router = express.Router();
 
 router.get('/csrf/token', (req: any, res: any, next: any) => {
   // generate csrf token
   res.send({csrfToken: req.csrfToken()});
+});
+
+router.post('/logout', (req: any, res: any, next: any) => {
+  req.logout();
+  console.log(res.headers);
+  res.redirect(`${config.frontEndUrl}/login`);
 });
 
 export const auth = router;

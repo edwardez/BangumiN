e1c5dd8042d009383075c7004ed92b4386d235c7
edward <11089470+edwardez@users.noreply.github.com>
2019-04-21 11:34:21 -0700

    Add a delay for async exception simulator

--- app/lib/shared/utils/misc/probability.dart
+++ app/lib/shared/utils/misc/probability.dart
@@ -3,11 +3,17 @@ import 'dart:math' as math;
 import 'package:munin/shared/exceptions/exceptions.dart';
 
 /// Mocks exception and throws an [GeneralUnknownException] by default with
-/// a 50% chance
+/// a 50% chance and a delay
 /// failurePercentage must be between 0 and 100(both inclusive)
-throwExceptionAtPercentage<T>([int failurePercentage = 50, T error]) {
+/// It's useful for simulating a http async operation
+throwExceptionAtPercentageWithDelay<T>(
+    {int failurePercentage = 50, T error, delaySeconds = 0}) async {
   assert(failurePercentage >= 0 && failurePercentage <= 100);
   error ??= GeneralUnknownException('Unknown Error') as T;
+
+  if (delaySeconds >= 0) {
+    await Future.delayed(Duration(seconds: delaySeconds));
+  }
 
   if (returnsTrueAtPercentage(failurePercentage)) {
     throw error;

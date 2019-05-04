4f8c7439b1afa06425143113a9c9c1057f62c9e1
edward <11089470+edwardez@users.noreply.github.com>
2019-05-03 21:50:21 -0700

    Update status update multiple widget to use circle avatar for user

--- app/lib/widgets/Timeline/TimelineItem/StatusUpdateMultipleWidget.dart
+++ app/lib/widgets/Timeline/TimelineItem/StatusUpdateMultipleWidget.dart
@@ -1,31 +1,48 @@
 import 'package:built_collection/built_collection.dart';
 import 'package:flutter/material.dart';
 import 'package:munin/models/bangumi/timeline/StatusUpdateMultiple.dart';
+import 'package:munin/models/bangumi/timeline/common/BangumiContent.dart';
 import 'package:munin/models/bangumi/timeline/common/HyperImage.dart';
 import 'package:munin/widgets/Timeline/TimelineItem/common/TimelineUserListTile.dart';
 import 'package:munin/widgets/Timeline/TimelineItem/common/WrappableHyperTextLinkList.dart';
+import 'package:munin/widgets/shared/avatar/CachedCircleAvatar.dart';
 import 'package:munin/widgets/shared/images/RoundedImageWithVerticalText.dart';
 
 /// see [StatusUpdateMultiple] for further explanation
 class StatusUpdateMultipleWidget extends StatelessWidget {
+  static const double imagePadding = 2.0;
   final StatusUpdateMultiple statusUpdateMultiple;
 
   const StatusUpdateMultipleWidget({Key key, @required this.statusUpdateMultiple})
       : super(key: key);
 
-  List<RoundedImageWithVerticalText> _buildImageLists(
-      BuiltList<HyperImage> images) {
-    List<RoundedImageWithVerticalText> imageWidgets = [];
-    for (var image in images) {
-      imageWidgets.add(RoundedImageWithVerticalText(
-        verticalImagePadding: 2.0,
-        contentType: image.contentType,
-        imageUrl: image.imageUrl,
-        id: image.id,
-        pageUrl: image.pageUrl,
-        totalWidthFactor: 1,
-      ));
+  List<Widget> _buildImageLists(BuiltList<HyperImage> images) {
+    print(statusUpdateMultiple.contentType);
+    List<Widget> imageWidgets = [];
+    if (statusUpdateMultiple.contentType == BangumiContent.User) {
+      for (var image in images) {
+        imageWidgets.add(Padding(
+          padding: const EdgeInsets.only(right: imagePadding),
+          child: CachedCircleAvatar(
+            imageUrl: image.imageUrl,
+            username: image.id,
+            navigateToUserRouteOnTap: true,
+          ),
+        ));
+      }
+    } else {
+      for (var image in images) {
+        imageWidgets.add(RoundedImageWithVerticalText(
+          verticalImagePadding: imagePadding,
+          contentType: image.contentType,
+          imageUrl: image.imageUrl,
+          id: image.id,
+          pageUrl: image.pageUrl,
+          totalWidthFactor: 1,
+        ));
+      }
     }
+
 
     return imageWidgets;
   }

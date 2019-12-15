d9e8dbd534a337dcbea71a2031eaf787452d0913
edwardez <11089470+edwardez@users.noreply.github.com>
2019-12-14 22:06:06 -0800

    Update config

--- app/ios/Flutter/flutter_export_environment.sh
+++ app/ios/Flutter/flutter_export_environment.sh
@@ -2,10 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/edward/bin/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/edward/code/BangumiN/app"
-export "FLUTTER_TARGET=/Users/edward/code/BangumiN/app/lib/config/development.dart"
+export "FLUTTER_TARGET=/Users/edward/code/BangumiN/app/lib/config/production.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/edward/bin/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/edward/bin/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=0.4.3"
 export "FLUTTER_BUILD_NUMBER=13"
 export "TRACK_WIDGET_CREATION=true"

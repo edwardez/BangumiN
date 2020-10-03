91b391725caed933936ec318d742d0ce4364b440
edwardez <11089470+edwardez@users.noreply.github.com>
2020-10-03 15:23:43 -0700

    Use sf symbol on homepage.

--- app/lib/widgets/home/MuninBottomNavigationBar.dart
+++ app/lib/widgets/home/MuninBottomNavigationBar.dart
@@ -1,9 +1,10 @@
 import 'package:badges/badges.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:munin/shared/utils/common.dart';
 import 'package:munin/widgets/shared/utils/Scroll.dart';
 import 'package:munin/widgets/shared/utils/common.dart';
 import 'package:munin/widgets/user/notification/NotificationIcon.dart';
-import 'package:outline_material_icons/outline_material_icons.dart';
 
 class MuninBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
@@ -21,16 +22,30 @@ class MuninBottomNavigationBar extends StatelessWidget {
       currentIndex: currentIndex,
       type: BottomNavigationBarType.fixed,
       items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(OMIcons.home), title: Text('动态')),
-        BottomNavigationBarItem(icon: Icon(OMIcons.done), title: Text('进度')),
-        BottomNavigationBarItem(icon: Icon(OMIcons.group), title: Text('讨论')),
+        BottomNavigationBarItem(
+            icon: Icon(isCupertinoPlatform()
+                ? CupertinoIcons.home
+                : Icons.home_outlined),
+            label: '动态'),
+        BottomNavigationBarItem(
+            icon: Icon(isCupertinoPlatform()
+                ? CupertinoIcons.checkmark_alt
+                : Icons.done_rounded),
+            label: '进度'),
+        BottomNavigationBarItem(
+            icon: Icon(isCupertinoPlatform()
+                ? CupertinoIcons.person_2
+                : Icons.group_outlined),
+            label: '讨论'),
         BottomNavigationBarItem(
           icon: NotificationIcon(
-            child: Icon(OMIcons.person),
+            child: Icon(isCupertinoPlatform()
+                ? CupertinoIcons.profile_circled
+                : Icons.person_outline_rounded),
             position: BadgePosition.topRight(top: 0, right: 0),
             padding: EdgeInsets.all(4.0),
           ),
-          title: Text('主页'),
+          label: '我的',
         ),
       ],
       onTap: (int tappedIndex) {
